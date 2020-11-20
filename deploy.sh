c7e016da4c00955efe6df40debd09e087fcd974f
carstendirks <carstendirks@gmail.com>
2020-11-20 08:45:30 +0100

    next1

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker built -t carstendirks/multi-client:latest -t carstendirks/multi-client:$SHA -f ./client/Dockerfile ./client
-docker built -t carstendirks/multi-server:latest -t carstendirks/multi-server:$SHA -f ./server/Dockerfile ./server
-docker built -t carstendirks/multi-worker:latest -t carstendirks/multi-worker:$SHA  -f ./worker/Dockerfile ./worker
+docker build -t carstendirks/multi-client:latest -t carstendirks/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t carstendirks/multi-server:latest -t carstendirks/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t carstendirks/multi-worker:latest -t carstendirks/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push carstendirks/multi-client:latest
 docker push carstendirks/multi-server:latest
@@ -11,7 +11,6 @@ docker push carstendirks/multi-server:$SHA
 docker push carstendirks/multi-worker:$SHA
 
 kubectl apply -f k8s
-
 kubectl set image deployments/server-deployment server=carstendirks/multi-server:$SHA
-kubectl set image deployments/client-deployment server=carstendirks/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=carstendirks/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=carstendirks/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=carstendirks/multi-worker:$SHA
